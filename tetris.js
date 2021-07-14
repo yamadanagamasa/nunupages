a3043261ee17dffc3bd54a1c5519b59dfd2da7c5
danieru <danieru565612345@gmail.com>
2021-07-14 19:38:05 +0900

    s

--- tetris.js
+++ tetris.js
@@ -97,6 +97,13 @@ class Tetris {
                 highlight: "rgb(255, 255, 255)",
                 shadow: "rgb(128, 82, 0)"
             },
+             {
+                shape: [[[-1, 0]],[[-1, 0]],[[-1, 0]],[[-1, 0]],
+                      ],
+                color: "rgb(0, 255, 255)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(0, 128, 128)"
+            },
             {
                 shape: [[[0, -1], [-1, 0], [0, 0], [1, 0]],
                         [[0, -1], [0, 0], [1, 0], [0, 1]],
@@ -321,3 +328,39 @@ class Tetris {
         context.fillRect(0, 0, canvas.width, canvas.height);
     }
 }
+var slideConts = document.querySelectorAll('.slideConts'); // スライドで表示させる要素の取得
+var slideContsRect = []; // 要素の位置を入れるための配列
+var slideContsTop = []; // 要素の位置を入れるための配列
+var windowY = window.pageYOffset; // ウィンドウのスクロール位置を取得
+var windowH = window.innerHeight; // ウィンドウの高さを取得
+var remainder = 100; // ちょっとはみ出させる部分
+
+// 要素の位置を取得
+for (var i = 0; i < slideConts.length; i++) {
+  slideContsRect.push(slideConts[i].getBoundingClientRect());
+}
+for (var i = 0; i < slideContsRect.length; i++) {
+  slideContsTop.push(slideContsRect[i].top + windowY);
+}
+
+// ウィンドウがリサイズされたら、ウィンドウの高さを再取得
+window.addEventListener('resize', function () {
+  windowH = window.innerHeight;
+});
+
+// スクロールされたら
+window.addEventListener('scroll', function () {
+  // スクロール位置を取得
+  windowY = window.pageYOffset;
+  
+  for (var i = 0; i < slideConts.length; i++) {
+    // 要素が画面の下端にかかったら
+    if(windowY > slideContsTop[i] - windowH + remainder) {
+      // .showを付与
+      slideConts[i].classList.add('show');
+    } else {
+      // 逆に.showを削除
+      slideConts[i].classList.remove('show');
+    }
+  }
+});
